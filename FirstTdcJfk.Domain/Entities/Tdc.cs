b1c41a45770636891853cb70cd9bdb899deaba83
Santiago Sierra Herrera <santiago.sierra@sofka.com.co>
2023-08-09 16:50:45 -0500

    Firts commit: Application and Domain Layers

---
+++ FirstTdcJfk.Domain/Entities/Tdc.cs
@@ -0,0 +1,25 @@
+﻿using FirstTdcJfk.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTdcJfk.Domain.Entities
+{
+    public class Tdc
+    {
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+        public string Category { get; set; }
+        public string Franchise { get; set; }
+        public string GoodThru { get; set; }
+        public string ClientName { get; set; }
+        public string CutoffDate { get; set; }
+        public string PaymentDate { get; set; }
+        public decimal Limit { get; set; }
+        public decimal Spent { get; set; }
+        public decimal Rate { get; set; }
+        public CardStatus Status { get; set; }
+    }
+}
---
+++ FirstTdcJfk.Domain/Enums/CardStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTdcJfk.Domain.Enums
+{
+    public enum CardStatus
+    {
+        Active,
+        Inactive,
+        Blocked,
+        Cancelled
+    }
+}
