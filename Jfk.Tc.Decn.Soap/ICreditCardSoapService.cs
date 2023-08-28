8d573a213ac9e88e93d8670b0ea0dc6c75a56fb0
fabian.zapata@sofka.com.co <fabian.zapata@sofka.com.co>
2023-08-28 17:17:29 -0500

    se adiciona proyecto soap

---
+++ Jfk.Tc.Decn.Soap/ICreditCardSoapService.cs
@@ -0,0 +1,23 @@
+﻿using Jfk.Tc.Decn.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.ServiceModel;
+
+namespace Jfk.Tc.Decn.Soap
+{
+    [ServiceContract, XmlSerializerFormat]
+    public interface ICreditCardSoapService
+    {
+        [OperationContract]
+        Task<IEnumerable<CreditCard>> GetAll();
+
+        [OperationContract]
+        Task<CreditCard> GetById(int id);
+
+        [OperationContract]
+        Task<int> Add(CreditCard tdc);
+        //Task<int> Add(ActionResult tdc);
+
+        [OperationContract]
+        string Res(int num1, int num2);
+    }
+}
